9690e85ac6836d71a342218092633f15cea71fca
Garv Kadia <145740289+garv0439@users.noreply.github.com>
2024-01-17 15:58:07 +0530

    Delete day1 directory

--- day1/script1.js
+++
@@ -1,7 +0,0 @@
-let namasteBtn = document.querySelector('button');
-namasteBtn.addEventListener('click', inputMsg);
-
-function inputMsg() {
-    let name = prompt('Enter Name of Student');
-    namasteBtn.textContent = 'Roll No. 1:' + name;
-}
