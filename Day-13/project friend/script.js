d0da3fd31aa610c60671784516d78d6345bb335d
Garv Kadia <145740289+garv0439@users.noreply.github.com>
2024-02-02 16:12:52 +0530

    Add files via upload

---
+++ Day-13/project friend/script.js
@@ -0,0 +1,6 @@
+let namasteBtn = document.querySelector('button');
+namasteBtn.addEventListener('click', inputMsg);
+
+function inputMsg() {
+    namasteBtn.textContent = 'Bhai Goa chal te hai!';
+}
---
+++ Day-13/project love/script.js
@@ -0,0 +1,6 @@
+let namasteBtn = document.querySelector('button');
+namasteBtn.addEventListener('click', inputMsg);
+
+function inputMsg() {
+    namasteBtn.textContent = 'I Love You Too!💕';
+}
