7513b7e76afc626c947e764fd0115185d23f2e89
Garv Kadia <145740289+garv0439@users.noreply.github.com>
2024-05-06 15:59:33 +0530

    /Day-9/

---
+++ Day-9/script.js
@@ -0,0 +1,56 @@
+console.log("garv")
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+const texts = document.querySelector('.text');
+const header1 = document.querySelector('h1');
+const header2 = document.querySelector('h2');
+const header3 = document.querySelector('h3');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (event) {
+    console.log(event);
+    console.log(event.target);
+    if (event.target.id === 'gray') {
+      body.style.backgroundColor = event.target.id;
+      texts.style.color = 'lightgreen'
+      header1.style.color = 'black'
+      header2.style.color = 'gold'
+      header3.style.color = 'blue'
+    }
+
+    if (event.target.id === 'white') {
+      body.style.backgroundColor = event.target.id;
+      texts.style.color = 'blue'
+      header1.style.color = 'black'
+      header2.style.color = 'red'
+      header3.style.color = 'gray'
+    }
+
+    if (event.target.id === 'blue') {
+      body.style.backgroundColor = event.target.id;
+      texts.style.color = 'violet'
+      header1.style.color = 'lightgreen'
+      header2.style.color = 'black'
+      header3.style.color = 'lightblue'
+    }
+
+    if (event.target.id === 'black') {
+      body.style.backgroundColor = event.target.id;
+      texts.style.color = 'wheat'
+      header1.style.color = 'green'
+      header2.style.color = 'gold'
+      header3.style.color = 'lightblue'
+    }
+
+    if (event.target.id === 'green') {
+      body.style.backgroundColor = 'rgb(72, 255, 48)';
+      texts.style.color = 'black'
+      header1.style.color = 'red'
+      header2.style.color = 'rgb(72, 255, 48)'
+      header3.style.color = 'darkblue'
+    }
+
+
+  });
+});
